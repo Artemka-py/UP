e045b3b4e3d3f9dc55da21329727d692fe117b20
Artemka-py <djartlyt@gmail.com>
2020-04-09 00:11:10 +0300

    first commit

---
+++ AistWPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AistWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string intID = "";
+        public static string FIOSotr = "";
+        public static Int32 intDropStatis = 0;
+        public static string strStatus = "Null";
+        public static bool connect = false;
+        private static Mutex _instanse;
+        private const string _app_name = "ИНЖПРОМТОРГ";
+
+        
+    }
+}
---
+++ AistWPF/Authorization.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using CryptLibraryING;
+
+namespace AistWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btReg_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            Close();
+        }
+
+        private void btEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = tbLogin.Text;
+            string Password = tbPassword.Text;
+            Table_Class tableClass = new Table_Class($"SELECT\r\n  Sotrudniki.ID_Sotrudnika\r\n ,Dolgnost.Dostup\r\nFROM dbo.Sotrudniki\r\nINNER JOIN dbo.Dolgnost\r\n  ON Sotrudniki.Dolgnost_ID = Dolgnost.ID_Dolgnost\r\n  WHERE Sotrudnika_Login = {Login} AND Sotrudnika_Password = {Password}");
+            if (tableClass.table.Rows[0][0] != DBNull.Value)
+            {
+                App.intID = tableClass.table.Rows[0][0].ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Не правильно введен логин или пароль!!!", "ИНЖПРОМТОРГ", MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+                tbPassword.Text = "";
+            }
+        }
+    }
+}
